5b7fb23061c9cd2b002fac57b10c5bc5cfb34a84
Daniel Minshew <ofenixculpa@gmail.com>
2016-03-04 12:29:17 -0600

    add PhotoSwipe

---
+++ assets/js/_pswp.js
@@ -0,0 +1,33 @@
+var openPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+		html: '<div class="hello-slide"><h1>Hello world <a href="http://example.com">example.com</a></h1></div>'
+	},
+        {
+            src: 'https://farm7.staticflickr.com/6175/6176698785_7dee72237e_b.jpg',
+            w: 1024,
+            h: 683
+        }
+    ];
+
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+openPhotoSwipe();
+
+document.getElementById('phsw_trig').onclick = openPhotoSwipe;
