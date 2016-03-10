b345daec65b6183cfd349d841138fcd4af68a5fd
Daniel Minshew <ofenixculpa@gmail.com>
2016-03-10 15:05:44 -0600

    more bg changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,13 +216,84 @@
 })(jQuery);
 
 $("#team-slider").owlCarousel({
-	pagination : false,
-	navigation: true,
-	responsive: true,
+	// pagination : false,
+	// navigation: true,
+	// responsive: true,
 	navigationText: [
 		"<img src='assets/img/swiper/arrow-l-dark.svg' class='arrow-left'>",
 		"<img src='assets/img/swiper/arrow-r-dark.svg' class='arrow-right'>"
-	]
+	],
+
+	// Most important owl features
+    items : 5,
+    itemsCustom : false,
+    itemsDesktop : [1199,4],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [768,2],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+
+    //Autoplay
+    autoPlay : false,
+    stopOnHover : false,
+
+    // Navigation
+    rewindNav : true,
+    scrollPerPage : false,
+
+    //Pagination
+    pagination : true,
+    paginationNumbers: false,
+
+    // Responsive
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+
+    // CSS Styles
+    baseClass : "owl-carousel",
+    theme : "owl-theme",
+
+    //Lazy load
+    lazyLoad : false,
+    lazyFollow : true,
+    lazyEffect : "fade",
+
+    //Auto height
+    autoHeight : false,
+
+    //JSON
+    jsonPath : false,
+    jsonSuccess : false,
+
+    //Mouse Events
+    dragBeforeAnimFinish : true,
+    mouseDrag : true,
+    touchDrag : true,
+
+    //Transitions
+    transitionStyle : false,
+
+    // Other
+    addClassActive : false,
+
+    //Callbacks
+    beforeUpdate : false,
+    afterUpdate : false,
+    beforeInit: false,
+    afterInit: false,
+    beforeMove: false,
+    afterMove: false,
+    afterAction: false,
+    startDragging : false,
+    afterLazyLoad : false
 });
 
 
